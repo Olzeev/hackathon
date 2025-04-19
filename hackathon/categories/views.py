4282456b74cbe8d243875a319a8b4d976ee046a8
Androfedos <gubanov.fedja@gmail.com>
2025-04-19 14:57:32 +0300

    become_helper

--- hackathon/categories/views.py
+++ hackathon/categories/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from .forms import HelperForm
 from categories.models import Helper
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Helper, Category
 
 def categories(request):
@@ -25,7 +25,22 @@ def categories(request):
 
 def auth(request):
     return render(request, 'categories/auth.html')
+
 def login(request):
     return render(request, 'categories/login.html')
+
 def logged(request):
     return render(request, 'categories/categories_logged.html')
+
+def become_helper(request):
+    if request.method == 'POST':
+        form = HelperForm(request.POST, request.FILES)
+        if form.is_valid():
+            helper = form.save(commit=False)
+            helper.is_online = False  # по умолчанию офлайн
+            helper.save()
+            form.save_m2m()  # сохранить категории
+            return redirect('categories')
+    else:
+        form = HelperForm()
+    return render(request, 'categories/become_helper.html', {'form': form})
