b182cac67e44808b73dff7588dbc4ea07963179e
Maxim <olzeevmax@gmail.com>
2025-04-19 17:52:27 +0300

    Fix

--- hackathon/chat/views.py
+++ hackathon/chat/views.py
@@ -6,19 +6,6 @@ from django.http import JsonResponse
 def index(request):
     return render(request, 'chat/chat.html')
 
-def login_user(request):
-    user = authenticate(request, username="olzeev", password="092531")
-    if user is not None:
-        # A backend authenticated the credentials
-        login(request, user)
-    else:
-        # No backend authenticated the credentials
-        ...
-    return redirect('main')
-
-def logout_user(request):
-    logout(request)
-    return redirect('main')
 
 def getUsername(request):
     if request.user.is_authenticated:
