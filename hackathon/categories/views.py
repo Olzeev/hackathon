17724641163a6532ef9cca50557a59f649af9e9d
warotify <karpov.vl.07@mail.ru>
2025-04-19 12:38:56 +0300

    bdadded

--- hackathon/categories/views.py
+++ hackathon/categories/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Helper
 
+# View to display all helpers as cards
+# Assumes template at 'categories.html'
 def categories(request):
-    return render(request, 'categories/categories.html')
+    # Получаем всех помощников из базы данных
+    helpers = Helper.objects.all()  # импорт модели Helper :contentReference[oaicite:0]{index=0}&#8203;:contentReference[oaicite:1]{index=1}
+    # Передаем их в шаблон для рендеринга
+    return render(request, 'categories/categories.html', {
+        'helpers': helpers,
+    })
