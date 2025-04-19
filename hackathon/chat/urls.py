bbeead57de5c00586fa47c50f4fa312d18ea46ec
Maxim <olzeevmax@gmail.com>
2025-04-19 17:43:08 +0300

    fix

--- hackathon/chat/urls.py
+++ hackathon/chat/urls.py
@@ -4,8 +4,5 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.index, name='main'), 
-    # login-section
-    path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout'),
     path('getUsername', views.getUsername, name='getUsername')
 ]
