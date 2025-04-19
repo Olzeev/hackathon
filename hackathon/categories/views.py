e413ee02e8d67433e68cb6bd1e25968b3a558d12
warotify <karpov.vl.07@mail.ru>
2025-04-19 14:30:12 +0300

    bdadded

--- hackathon/categories/views.py
+++ hackathon/categories/views.py
@@ -3,11 +3,26 @@ from django.shortcuts import render
 from categories.models import Helper
 
 
-# View to display only online helpers, sorted by descending rating
+from django.shortcuts import render
+from .models import Helper, Category
+
 def categories(request):
-    # Фильтруем помощников по статусу онлайн и сортируем по рейтингу (по убыванию)
-    helpers = Helper.objects.filter(is_online=True).order_by('-rate')
-    return render(request, 'categories/categories.html', {'helpers': helpers})
+    all_categories = Category.objects.all()
+    selected_cats = request.GET.getlist('categories')
+
+    helpers = Helper.objects.filter(is_online=True)
+
+    if selected_cats:
+        helpers = helpers.filter(categories__id__in=selected_cats).distinct()
+
+    helpers = helpers.order_by('-rate')
+
+    return render(request, 'categories/categories.html', {
+        'helpers': helpers,
+        'all_categories': all_categories,
+        'selected_cats': list(map(int, selected_cats)) if selected_cats else [],
+    })
+
 def auth(request):
     return render(request, 'categories/auth.html')
 def login(request):
