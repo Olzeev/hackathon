d54b02429bfa80cb3717a2d106db656a0c631d2f
iljas277 <ilaartamonov620@gmail.com>
2025-04-19 21:21:10 +0300

    authorization

--- hackathon/categories/urls.py
+++ hackathon/categories/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from chat.views import index
 
 urlpatterns = [
     path('', views.categories, name='categories'),
-    path('auth', views.auth, name = 'auth'),
-    path("register_view", views.register_view , name= 'register_view'),
-    path('login', views.login, name = 'login'),
-    path('categories_logged', views.logged, name = 'logged'),
-    path('', views.categories, name='categories'),
-    path('succesful_login', views.succesful_login, name = 'succesful_login'),
+    path('auth/', views.auth, name = 'auth'),
+    path('register_view', views.register_view, name = "register_view"),
     path('chat/<int:helper_id>/', index, name='chat_view'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
